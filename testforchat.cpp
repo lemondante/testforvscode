f2a4ef468085eb180c95c66b41f5b04260ec0696
lemondante <745558624@qq.com>
2021-01-21 23:09:47 +0800

    ttt

---
+++ testforchat.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void ywk_talk()
+{
+    problem1;
+    who has done?
+    any solutions?
+};
+//test thursday
+
+void ywk_talk()
+{
+    problem1;
+    who has done?
+    any solutions?
+};void ywk_talk()
+{
+    problem1;
+    who has done?
+    any solutions?
+};void ywk_talk()
+{
+    problem1;
+    who has done?
+    any solutions?
+};
