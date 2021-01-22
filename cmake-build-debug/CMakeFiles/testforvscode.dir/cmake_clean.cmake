e1846f12d3e668a51fb2a7703d00b4615f56a820
alicesla <2653861838@qq.com>
2021-01-22 19:24:40 +0800

    sla

---
+++ cmake-build-debug/CMakeFiles/testforvscode.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testforvscode.dir/sla/1.cpp.obj"
+  "CMakeFiles/testforvscode.dir/slz/1.cpp.obj"
+  "CMakeFiles/testforvscode.dir/testforchat.cpp.obj"
+  "CMakeFiles/testforvscode.dir/ywk/test.cpp.obj"
+  "libtestforvscode.dll.a"
+  "testforvscode.exe"
+  "testforvscode.exe.manifest"
+  "testforvscode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testforvscode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- sla/3.cpp
+++ sla/3.cpp
@@ -1,6 +1,8 @@
 //
 // Created by 26538 on 2021/1/22.
 //
+//https://blog.csdn.net/weixin_44351043/article/details/104442577
+//lei si
 #include<iostream>
 using namespace std;
 int main(){
